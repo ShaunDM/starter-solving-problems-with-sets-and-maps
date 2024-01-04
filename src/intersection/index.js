0262147b731145725ff1e01d01b59bc5ac2f8f7e
ShaunDM <123577673+ShaunDM@users.noreply.github.com>
2024-01-04 14:16:38 -0500

    update
    Update after completing lessons related to repo

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,15 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    const mapA = new Map();
+    const setB = new Set();
+    for(let i = 0; i < a.length; i++){
+        mapA.set(a[i], i);
+    }
+    for(let e of b){
+        if(mapA.has(e)){
+            setB.add(e);
+        }
+    }
+    return [...setB];
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,26 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+    for(let i = 0; i < A.length; i++){
+        numbers.set(A[i], i);
+    }
+    for(let key of numbers.keys()){
+        const difference = N - key;
+        if(difference === key){
+            continue;
+        }
+        if(numbers.has(difference)){
+            if(key > difference){
+                solution.set(difference, key);
+            }else{
+                solution.set(key, difference);
+            }
+        }
+    }
+    return [...solution];
+}
 
 module.exports = sumPairs;
